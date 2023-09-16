237c4f2f2f537bb2b20d4534720466626ca7e650
Diego <idiegocsocs9@gmail.com>
2023-09-16 05:49:04 -0300

    Improving data types & NEW func: search an specific bus.

--- app/[id]/[bus]/page.tsx
+++ app/[id]/[bus]/page.tsx
@@ -25,15 +25,15 @@ type ParaderoData = {
   buses: Array<MicroBus>;
   valid: boolean
 };
-
-export default async function Page(path: { params: { id: string } }){
+//Esta pagina aparece solo en caso de que se quiera buscar un recorrido específico
+export default async function Page(path: { params: { id: string, bus:string } }){
   const paraderoRes = await fetch(`https://api.xor.cl/red/bus-stop/${path.params.id}`,{
     cache:"no-store",
   })
   const dataParadero = await paraderoRes.json()
-  console.log(dataParadero)
 
   let microsParadero
+
   if (dataParadero.status_code === 0) {
     // Ordenando dataParadero para mostrar primero los servicios en horario hábil con sort()
     microsParadero = dataParadero.services.sort((a: Services, b: Services) => {
@@ -47,6 +47,7 @@ export default async function Page(path: { params: { id: string } }){
       <main className="container-xxl d-flex flex-column pt-2 text-center align-items-center">
         <h1 className="row fw-normal">{dataParadero.name}</h1>
         <h2 className="row fw-bold text-light bg-success p-1 rounded-1">ID: {dataParadero.id}</h2>
+        <h3 className="row fw-bold text-dark bg-info p-1 rounded-1">Bus: {path.params.bus.toUpperCase()}</h3>
         <table className="table table-dark table-striped">
           <thead>
           <tr>
@@ -59,14 +60,13 @@ export default async function Page(path: { params: { id: string } }){
           <tbody>
 
           {microsParadero?.map((data: ParaderoData) => {
-            console.log(data)
-            if(data.valid === true){
+
+            if(data.valid === true && data.id === path.params.bus.toUpperCase()){
               return(
                 <>
                   {data.buses.map((dataBus) =>{
                     let distanceText
                     const patente:string = dataBus.id
-                    console.log(dataBus)
                     if (dataBus.min_arrival_time > 0 && dataBus.max_arrival_time >= 5 ) {
                       distanceText = `${dataBus.min_arrival_time} y ${dataBus.max_arrival_time} min`
                     }
@@ -94,7 +94,7 @@ export default async function Page(path: { params: { id: string } }){
                 }
               </>
             )
-          } else if(data.valid === false){
+          } else if(data.valid === false && data.id === path.params.bus.toUpperCase()){
               return(
                 <tr className={styles.noStriped} key={data.id}>
                   <td className="fw-bold" key={data.id+data.id}>{data.id}</td>
@@ -106,16 +106,17 @@ export default async function Page(path: { params: { id: string } }){
         )}
         </tbody>
       </table>
-      <a className="btn btn-success w-75 fw-bold align-self-center mb-5" href="./">Click para volver al inicio</a>
+      <a className="btn btn-success w-75 fw-bold align-self-center mb-5" href=".././">Click para volver al inicio</a>
     </main>
     )
-  } else {
+  }
+  else {
       return(
         <div className="d-flex flex-column bg-warning vh-100 text-center">
           <h1 className="fw-bolder text-bg-danger display-1 p-5">ERROR:</h1>
           <h2 className="display-3 p-4">{dataParadero.status_description}
           </h2>
-          <a className="btn btn-lg btn-light fw-bold align-self-center" href="./">Volver al inicio</a>
+          <a className="btn btn-lg btn-light fw-bold align-self-center" href=".././">Volver al inicio</a>
         </div>
     )
   }
--- components/ParaderoForm.tsx
+++ components/ParaderoForm.tsx
@@ -9,26 +9,40 @@ const ParaderoForm = () => {
 
   const router = useRouter();
 
-  const [inputValue, setInputValue] = useState("");
-  const [inputHistory, setInputHistory] = useState<string[]>([]);
+  const [InputParaderoValue, setInputParaderoValue] = useState("");
+  const [ParaderoHistory, setParaderoHistory] = useState<string[]>([]);
+
+  const [InputServicioValue, setInputServicioValue] = useState("");
+  const [ServicioHistory, setServicioHistory] = useState<string[]>([]);
 
   // Obtener historial de valores del localStorage al cargar el componente
   useEffect(() => {
-    const storedInputHistory = localStorage.getItem("inputHistory");
-    if (storedInputHistory) {
-      setInputHistory(JSON.parse(storedInputHistory).slice(-5));
+    const storedParaderoHistory = localStorage.getItem("ParaderoHistory");
+    if (storedParaderoHistory) {
+      setParaderoHistory(JSON.parse(storedParaderoHistory).slice(-5));
+    }
+
+    const storedServicioHistory = localStorage.getItem("ServicioHistory");
+    if (storedServicioHistory) {
+      setServicioHistory(JSON.parse(storedServicioHistory).slice(-5));
     }
   }, []);
 
   const handleSubmit = (e: React.FormEvent <HTMLFormElement>) => {
     e.preventDefault();
-    router.push(inputValue);
+    const ruta = `${InputParaderoValue}/${InputServicioValue}`
+    router.push(ruta);
     setLoading(true);
     // Agregar valor actual al historial de valores si no existe ya
-    if (!inputHistory.includes(inputValue)){
-      setInputHistory([...inputHistory.slice(-4), inputValue]);
-      // Guardar nuevo historial de valores en el localStorage
-      localStorage.setItem("inputHistory", JSON.stringify([...inputHistory.slice(-4), inputValue]));
+    // Guardar nuevo historial de valores en el localStorage
+    if (!ParaderoHistory.includes(InputParaderoValue)){
+      setParaderoHistory([...ParaderoHistory.slice(-4), InputParaderoValue]);
+      localStorage.setItem("ParaderoHistory", JSON.stringify([...ParaderoHistory.slice(-4), InputParaderoValue]));
+    }
+
+    if (!ServicioHistory.includes(InputServicioValue)){
+      setServicioHistory([...ServicioHistory.slice(-4), InputServicioValue]);
+      localStorage.setItem("ServicioHistory", JSON.stringify([...ServicioHistory.slice(-4), InputServicioValue]));
     }
   }
 
@@ -43,14 +57,38 @@ const ParaderoForm = () => {
           type="text"
           placeholder='Ej: PF210'
           /* Redefinimos el valor de la ruta */
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
+          value={InputParaderoValue}
+          pattern="[a-zA-Z0-9]*"
+          onChange={(event) => setInputParaderoValue(event.target.value)}
           /* Asociamos el datalist al input */
-          list="inputHistory"
+          list="ParaderoHistory"
         />
+        <h2>Servicio (OPCIONAL)</h2>
+        <input
+          minLength={3}
+          maxLength={6}
+          className='form-control form-control-lg'
+          type="text"
+          placeholder='Ej: 210'
+          /* Redefinimos el valor de la ruta */
+          value={InputServicioValue}
+          pattern="[a-zA-Z0-9]*"
+          onChange={(event) => setInputServicioValue(event.target.value)}
+          /* Asociamos el datalist al input */
+          list="ServicioHistory"
+        />
+
+        {/* Advertencia en caso que no se respete el formato */}
+        {(!/^[a-zA-Z0-9]*$/.test(InputParaderoValue) || !/^[a-zA-Z0-9]*$/.test(InputServicioValue)) && (
+          <span className="text-danger fw-bold">Solo se permiten letras y números.</span>
+        )}
+
         {/* Creamos el datalist con las opciones de historial */}
-        <datalist id="inputHistory">
-          {inputHistory.map((value, index) => <option key={index} value={value} />)}
+        <datalist id="ParaderoHistory">
+          {ParaderoHistory.map((value, index) => <option key={index} value={value} />)}
+        </datalist>
+        <datalist id="ServicioHistory">
+          {ServicioHistory.map((value, index) => <option key={index} value={value} />)}
         </datalist>
         <button
           className='btn btn-primary btn-lg' type="submit"
