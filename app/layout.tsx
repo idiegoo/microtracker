48f52f1be9fc1251f97377ed2125c310d70143a2
Diego <idiegocsocs9@gmail.com>
2023-10-09 13:55:54 -0300

    Analytics added and updates of SEO & Next v13.5.4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import './globals.css'
+import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react';
+
+export const metadata: Metadata = {
+  title: 'Consulta paradero',
+  description: 'Página para consultar en cuanto llega la micro al paradero consumiendo la API de Red.',
+  authors: {name: 'Diego Ramirez', url:'https://diegoramirez.vercel.app'},
+}
 
 export default function RootLayout({
   children,
@@ -7,12 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <head/>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
